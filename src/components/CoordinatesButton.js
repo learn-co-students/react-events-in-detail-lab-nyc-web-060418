3e71c0f7d641219f462e5b0c0ab91791efa19bf7
Mary-Kate <marykatj@gmail.com>
2018-08-08 10:00:29 -0400

    Done.

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,1 +1,28 @@
 // Code CoordinatesButton Component Here
+
+import React, { Component } from 'react'
+import DelayedButton from './DelayedButton'
+
+class CoordinatesButton extends Component {
+
+  constructor(props) {
+    super(props)
+
+  }
+
+  arrayCreate = (event) => {
+        this.props.onReceiveCoordinates([event.clientX, event.clientY])
+  }
+
+  render() {
+    return (
+      <button onClick={this.arrayCreate}>Coords</button>
+    )
+  }
+}
+
+export default CoordinatesButton;
+
+//props: onReceiveCoordinates. This prop is a function passed down from index.js. This function is currently just logging whatever is passed into it.
+
+// create an array with two elements: the X and Y coordinates of the mouse. Find these using the event data.
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,30 @@
 // Code DelayedButton Component Here
+import React, {Component} from 'react'
+import CoordinatesButton from './CoordinatesButton'
+
+class DelayedButton extends Component {
+
+  constructor(props) {
+    super(props)
+
+  }
+
+  clickEvent = (event) => {
+    event.persist();
+    setTimeout(() => {
+      this.props.onDelayedClick(event);
+    }, this.props.delay);
+  }
+
+  render() {
+    return (
+      <button onClick={this.clickEvent}>Delayed</button>
+    )
+  }
+}
+
+export default DelayedButton;
+
+//two props: onDelayedClick (a function), and delay (a number).
+
+//will pass the click event to the onDelayedClick prop within a setTimeout(). The setTimeout() should be set to this.props.delay.
